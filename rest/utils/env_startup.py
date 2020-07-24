def41d3200377dd1a4185cd635ec4293308c66cc
Catalin Dinuta <43060213+dinuta@users.noreply.github.com>
2020-07-24 14:34:12 +0300

    corrected var to be string (#23)

--- rest/utils/env_startup.py
+++ rest/utils/env_startup.py
@@ -15,8 +15,7 @@ class EnvStartup:
             "app_ip_port": os.environ.get('APP_IP_PORT').strip().lower() if os.environ.get('APP_IP_PORT') else None,
             "port": int(os.environ.get('PORT').strip()) if os.environ.get('PORT') is not None else 8080,
             "eureka_server": os.environ.get('EUREKA_SERVER').strip() if os.environ.get('EUREKA_SERVER') else None,
-            "fluentd_ip_port": int(os.environ.get('FLUENTD_IP_PORT').strip()) if os.environ.get(
-                'FLUENTD_IP_PORT') else None,
+            "fluentd_ip_port": os.environ.get('FLUENTD_IP_PORT').strip() if os.environ.get('FLUENTD_IP_PORT') else None,
             "http_auth_token": os.environ.get('HTTP_AUTH_TOKEN').strip() if os.environ.get(
                 'HTTP_AUTH_TOKEN') else "None"
         }
