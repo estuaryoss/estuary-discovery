e40c627501c865b35ebf95b7b1e0261ba8d06e2c
Catalin Dinuta <43060213+dinuta@users.noreply.github.com>
2021-03-07 19:22:06 +0200

    updated eureka and image names (#12)

--- docker_push_tag.sh
+++ docker_push_tag.sh
@@ -3,13 +3,13 @@
 echo "$DOCKERHUB_TOKEN" | docker login -u "$DOCKERHUB_USERNAME" --password-stdin
 
 #centos
-docker build -t dinutac/estuary-discovery-centos:"${TRAVIS_TAG}" -f Dockerfile_centos .
-docker push dinutac/estuary-discovery-centos:"${TRAVIS_TAG}"
+docker build -t estuaryoss/discovery-centos:"${TRAVIS_TAG}" -f Dockerfile_centos .
+docker push estuaryoss/discovery-centos:"${TRAVIS_TAG}"
 
 #for alpine clean everything
 git reset --hard && git clean -dfx
 git checkout tags/"${TRAVIS_TAG}" -b "${TRAVIS_TAG}"
 
 #alpine
-docker build . -t dinutac/estuary-discovery:"${TRAVIS_TAG}"
-docker push dinutac/estuary-discovery:"${TRAVIS_TAG}"
+docker build . -t estuaryoss/discovery:"${TRAVIS_TAG}"
+docker push estuaryoss/discovery:"${TRAVIS_TAG}"
