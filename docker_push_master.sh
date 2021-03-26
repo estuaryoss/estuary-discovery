5c1d3f1bfa3770e7065d1f8ea62443a944c422a9
Catalin Dinuta <constantin.dinuta@gmail.com>
2021-03-26 17:56:40 +0200

    removed alpine image

--- docker_push_master.sh
+++ docker_push_master.sh
@@ -3,13 +3,9 @@
 echo "$DOCKERHUB_TOKEN" | docker login -u "$DOCKERHUB_USERNAME" --password-stdin
 
 #centos
-docker build -t estuaryoss/discovery-centos:latest -f Dockerfile_centos .
-docker push estuaryoss/discovery-centos:latest
+docker build -t estuaryoss/discovery:latest -f Dockerfile .
+docker push estuaryoss/discovery:latest
 
 #for alpine clean everything
 git reset --hard && git clean -dfx
 git checkout "${TRAVIS_BRANCH}"
-
-#alpine
-docker build . -t estuaryoss/discovery:latest
-docker push estuaryoss/discovery:latest
