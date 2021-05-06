dad4efaa22c02487448da92f8cbc6d3d0c77ddac
Catalin Dinuta <constantin.dinuta@gmail.com>
2021-05-06 21:33:48 +0300

    corrected tests

--- docker_push_tag.sh
+++ docker_push_tag.sh
@@ -2,10 +2,10 @@
 
 echo "$DOCKERHUB_TOKEN" | docker login -u "$DOCKERHUB_USERNAME" --password-stdin
 
+#compile
+pip3 install -r requirements.txt
+pyinstaller --onefile --clean main.py
+
 #centos
 docker build -t estuaryoss/discovery:"${TRAVIS_TAG}" -f Dockerfile .
 docker push estuaryoss/discovery:"${TRAVIS_TAG}"
-
-#for alpine clean everything
-git reset --hard && git clean -dfx
-git checkout tags/"${TRAVIS_TAG}" -b "${TRAVIS_TAG}"
